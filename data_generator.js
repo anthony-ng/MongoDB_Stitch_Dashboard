454a6f1e9bb5f7bd4c9dcf75a67d3afcc331a1f3
Anthony Ng <thomsonng123@gmail.com>
2017-10-13 01:41:03 -0700

    add express

--- data_generator.js
+++ data_generator.js
@@ -47,8 +47,8 @@ client.login().then(generateReceipts);
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
+
+    response.render('index');
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
